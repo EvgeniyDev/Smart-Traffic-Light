82ea6bd4ee768163c03ae059d95b5a01a8ec66f6
EvgeniyDev <zh.hmelevskoj2013@gmail.com>
2020-05-24 00:34:42 +0300

    Global game settings
    - Added fullscreen, resolution, volume, graphics quality settings

---
+++ Smart Traffic Light/Assets/Scripts/MainMenuScripts/SettingsMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer mainMixer;
+
+    public TMP_Dropdown resolutionDropdown;
+    private Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutionDropdown.ClearOptions();
+        resolutions = Screen.resolutions;
+
+        var currentResolutionIndex = 0;
+        var resolutionList = new List<string>(16);
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            var option = $"{resolutions[i].width}x{resolutions[i].height}";
+            resolutionList.Add(option);
+
+            if (resolutions[i].width == Screen.width
+                && resolutions[i].height == Screen.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(resolutionList);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        var resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetVolume(float volume)
+    {
+        mainMixer.SetFloat("volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
