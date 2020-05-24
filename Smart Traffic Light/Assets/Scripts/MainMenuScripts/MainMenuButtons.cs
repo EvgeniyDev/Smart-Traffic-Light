38462fc9a04438d4aa867004e76ed235666f22e2
EvgeniyDev <zh.hmelevskoj2013@gmail.com>
2020-05-25 02:03:49 +0300

    Final fixes
    - Added images to level images
    - Unmuted music
    - Added audio listener on camera on 2 level

--- Smart Traffic Light/Assets/Scripts/MainMenuScripts/MainMenuButtons.cs
+++ Smart Traffic Light/Assets/Scripts/MainMenuScripts/MainMenuButtons.cs
@@ -6,7 +6,7 @@ public class MainMenuButtons : MonoBehaviour
     // Hardcoded parameter value
     public void LoadFirstLevel() => SceneManager.LoadScene("FirstLevel");
 
-    public void LoadSecondLevel() => throw new System.NotImplementedException();
+    public void LoadSecondLevel() => SceneManager.LoadScene("SecondLevel");
 
     public void QuitGame() => Application.Quit();
 }
